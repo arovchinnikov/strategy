0a2d89f9274ab9ebdf01b01f8e9af3f085134ac9
Artem Ovchinnikov <arovchinnikov@icloud.com>
2025-02-27 20:52:04 +0300

    mesh_map

--- src/core/map/mod.rs
+++ src/core/map/mod.rs
@@ -1,9 +1,11 @@
+use std::path::Path;
 use bevy::app::{App, Plugin, Startup};
 use bevy::asset::{Assets, RenderAssetUsages};
 use bevy::pbr::wireframe::{Wireframe};
 use bevy::pbr::StandardMaterial;
 use bevy::prelude::{Camera3d, Color, Commands, Mesh, Mesh3d, MeshMaterial3d, ResMut, Transform, Vec3};
 use bevy::render::mesh::{Indices, PrimitiveTopology};
+use image::{GrayImage, ImageReader};
 
 pub struct MapPlugin;
 
@@ -28,11 +30,11 @@ pub fn init(
     let width = 8192.0;
     let height = 3616.0;
     let subdivisions_x = 256;
+    let subdivisions_z = 256;
 
-    // Сохраним пропорции плоскости:
-    let subdivisions_y = ((subdivisions_x as f32) * (height / width)).round() as u32;
+    let heightmap = load_image_sync("common/map/heightmap.png").into_luma8();
 
-    let plane_mesh = generate_plane(width, height, subdivisions_x, subdivisions_y);
+    let plane_mesh = generate_terrain_mesh(0, 0, width, height, subdivisions_x, subdivisions_z, &heightmap);
 
     commands.spawn((
         Mesh3d::from(meshes.add(plane_mesh)),
@@ -42,39 +44,46 @@ pub fn init(
         })),
         Transform {
             ..Default::default()
-        },
-        Wireframe
+        }
     ));
 }
 
-fn generate_plane(width: f32, height: f32, subdivisions_x: u32, subdivisions_y: u32) -> Mesh {
+fn generate_terrain_mesh(
+    start_x: u32,
+    start_z: u32,
+    width: f32,
+    height: f32,
+    subdivisions_x: u32,
+    subdivisions_z: u32,
+    heightmap: &GrayImage
+) -> Mesh {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default());
-    let vert_count = ((subdivisions_x + 1) * (subdivisions_y + 1)) as usize;
+    let vert_count = ((subdivisions_x + 1) * (subdivisions_z + 1)) as usize;
     let mut positions = Vec::with_capacity(vert_count);
     let mut normals = Vec::with_capacity(vert_count);
     let mut uvs = Vec::with_capacity(vert_count);
 
     // Определяем шаг по каждой оси и начальную позицию (центрированная плоскость)
     let step_x = width / subdivisions_x as f32;
-    let step_y = height / subdivisions_y as f32;
+    let step_y = height / subdivisions_z as f32;
     let origin_x = -width / 2.0;
     let origin_y = -height / 2.0;
 
     // Генерируем вершины
-    for y in 0..=subdivisions_y {
+    for y in 0..=subdivisions_z {
         for x in 0..=subdivisions_x {
             let pos_x = origin_x + x as f32 * step_x;
             let pos_y = 0.0;
             let pos_z = origin_y + y as f32 * step_y;
             positions.push([pos_x, pos_y, pos_z]);
             normals.push([0.0, 1.0, 0.0]); // нормаль вверх
-            uvs.push([x as f32 / subdivisions_x as f32, y as f32 / subdivisions_y as f32]);
+            uvs.push([x as f32 / subdivisions_x as f32, y as f32 / subdivisions_z as f32]);
         }
     }
 
     // Генерируем индексы для треугольников
     let mut indices = Vec::new();
-    for y in 0..subdivisions_y {
+    for y in 0..subdivisions_z {
         for x in 0..subdivisions_x {
             let i = y * (subdivisions_x + 1) + x;
             // Первый треугольник
@@ -93,4 +102,13 @@ fn generate_plane(width: f32, height: f32, subdivisions_x: u32, subdivisions_y: 
     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
     mesh.insert_indices(Indices::U32(indices));
     mesh
+}
+
+fn load_image_sync(path: &str) -> image::DynamicImage {
+    let img = ImageReader::open(Path::new(path))
+        .expect("Failed to open image")
+        .decode()
+        .expect("Failed to decode image");
+
+    img
 }
